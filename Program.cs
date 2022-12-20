1885ff3d1998befeda8b6037d87d8d97774f495a
Andre Nakazawa <andrenkzw@gmail.com>
2022-12-20 19:02:24 -0300

    Questao 4 - Calculo de POV

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace POV;
+class Program
+{
+    static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
+        => (int)Math.Truncate(totalNegociado * porcentagem / (1 - porcentagem));
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(funcaoRetornaQuantitadePOV((decimal)0.1, 90));
+        Console.WriteLine(funcaoRetornaQuantitadePOV((decimal)0.1, 100));
+        Console.WriteLine(funcaoRetornaQuantitadePOV((decimal)0.2, 70));
+    }
+}
